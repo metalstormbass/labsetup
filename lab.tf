7f155cab595152544a8e39cfbf35a03bc4a3ff22
Michael Braun <michaelbraunbass@gmail.com>
2021-02-26 12:08:50 -0700

    update

--- lab.tf
+++ lab.tf
@@ -1,72 +1,48 @@
-
-locals {
-vpc_map = {
-vpc-1 = "10.1.0.0/24"
-vpc-2 = "10.2.0.0/24"
-vpc-3 = "10.3.0.0/24"
-vpc-4 = "10.4.0.0/24"
-vpc-5 = "10.5.0.0/24"
-vpc-6 = "10.6.0.0/24"
-vpc-7 = "10.7.0.0/24"
-vpc-8 = "10.8.0.0/24"
-vpc-9 = "10.9.0.0/24"
-vpc-10 = "10.10.0.0/24"
-vpc-11 = "10.11.0.0/24"
-vpc-12 = "10.12.0.0/24"
-}
-}
-
 #Build VPC
 resource "aws_vpc" "labvpc" {
-  for_each = locals.vpc_map
-  cidr_block = [each.value]
-
+  cidr_block = var.aws_vpc_cidr
 }
-
-/*
 
 # Build Internet Gateway
-resource "aws_internet_gateway" "waap_demo_gateway" {
-  vpc_id = aws_vpc.waapdemovpc.id
+resource "aws_internet_gateway" "lab_gateway" {
+  vpc_id = aws_vpc.labvpc.id
 }
-
 
 # Create a subnet
 resource "aws_subnet" "external" {
-  vpc_id                  = aws_vpc.waapdemovpc.id
+  vpc_id                  = aws_vpc.labvpc.id
   cidr_block              = var.aws_subnet_cidr
   availability_zone       = var.primary_az
  
 }
 
 #Create a Route Table
-resource "aws_route_table" "waap_route_table" {
-  vpc_id = aws_vpc.waapdemovpc.id
+resource "aws_route_table" "lab_route_table" {
+  vpc_id = aws_vpc.labvpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.waap_demo_gateway.id
+    gateway_id = aws_internet_gateway.lab_gateway.id
   }
   
   route {
     ipv6_cidr_block        = "::/0"
-    gateway_id = aws_internet_gateway.waap_demo_gateway.id
+    gateway_id = aws_internet_gateway.lab_gateway.id
   }
   
   }
-
+  
 #Associate Route Table to Subnet
-resource "aws_route_table_association" "waap_route_association" {
+resource "aws_route_table_association" "lab_route_association" {
     subnet_id = aws_subnet.external.id
-    route_table_id = aws_route_table.waap_route_table.id
+    route_table_id = aws_route_table.lab_route_table.id
 }
-
 
 #Create security groups 
 
-resource "aws_security_group" "waap_sg" {
-  name = "${var.victim_company}-ssh-sg"
-  vpc_id = aws_vpc.waapdemovpc.id
+resource "aws_security_group" "lab_sg" {
+  name = "lab_sg"
+  vpc_id = aws_vpc.labvpc.id
 ingress {
       from_port   = 22
       to_port     = 22
@@ -77,24 +53,29 @@ ingress {
       from_port   = 80
       to_port     = 80
       protocol    = "tcp"
-      cidr_blocks = [var.source_ip]
+      cidr_blocks = "0.0.0.0/0"
   }
   
 egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = [var.source_ip]
+    cidr_blocks = "0.0.0.0/0"
   }
 
 }
 
 #Create Network Interface
 resource "aws_network_interface" "waap-nic" {
+    count = var.instances
     subnet_id = aws_subnet.external.id
-    private_ips = [var.waap_private]
+    #private_ips = ""
     security_groups = [aws_security_group.waap_sg.id]
 }
+
+/*
+
+
 
 
 #Create Elastic IP
--- variables.tf
+++ variables.tf
@@ -4,4 +4,27 @@ variable "aws_region" {
   default     = "us-east-1"
 }
 
+#AWS VPC CIDR
+variable "aws_vpc_cidr" {
+  description = "aws vpc cidr"
+  type        = string
+  default     = "10.20.0.0/16"
+}
 
+#AWS AZ
+variable "primary_az" {
+  description = "primary AZ"
+  default     = "us-east-1a"
+}
+
+#AWS Subnet CIDR
+variable "aws_subnet_cidr" {
+  description = "aws subnet cidr"
+  type        = string
+  default     = "10.20.0.0/24"
+}
+#Number of instances
+variable "instances" {
+  description = "number of ec2 instances"
+  default = 12
+}
