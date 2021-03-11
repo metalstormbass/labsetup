72a9e172ea4dab6cf567be3377f92626d3e61c98
Michael Braun <michaelbraunbass@gmail.com>
2021-03-11 08:47:09 -0700

    update

--- lab.tf
+++ lab.tf
@@ -56,6 +56,12 @@ ingress {
       protocol    = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
   }
+ingress {
+      from_port   = 0
+      to_port     = 3389
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
   
 egress {
     from_port   = 0
